1b9d72c919066dbb6fb9a647a08609bc68b75c78
NaosAntares <phyzheye01@gmail.com>
2021-01-15 17:08:34 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./sass/main.scss";
+import Hero from "./assets/asset.jpg";
+import logo from "./assets/logo.png";
+import font from "./assets/font/Bagerich.ttf";
+import arrow from "./assets/arrow.png"
+import proj from "./assets/proj-img.png"
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+            // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]',
+        }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'url-loader'
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
